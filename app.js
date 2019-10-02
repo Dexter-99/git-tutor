e9239783b34ec3b54f74da4f9b2c00c9c5df134e
Jitendra Rajpoot <devansh.forlife@gmail.com>
2019-10-02 12:48:41 +0530

    Third Commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("Connected");
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", e => {
+  let num1 = Number(document.querySelector("#num1").value);
+  let num2 = Number(document.querySelector("#num2").value);
+  let sum = num1 + num2;
+  console.log(sum);
+  let result = (document.querySelector(".result").innerHTML = sum);
+});
